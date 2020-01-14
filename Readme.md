046e37a49aba71808bbc9e5043c2b9d9cc9893c9
Aman Singh <aman@cognitree.com>
2020-01-14 23:22:41 +0530

    Code structuring

---
+++ Readme.md
@@ -0,0 +1,12 @@
+#This is a custom threadpool.
+
+For more on threadpool you can visit these pages.
+ 1) [Wiki](https://en.wikipedia.org/wiki/Thread_pool)
+ 2) [GeeksforGeeks](http://github.com)
+ 
+##Explaination of code.
+* The main class creates 4 sample task.
+* The sample task sleeps for 2 secs and prints the task number.
+* The SIZE reffers to the no. of threads in the threadpool.
+* MyThreadPool is the implementation of my custom threadpool.
+ 
--- src/main/java/com/aman/customthreadpool/Main.java
+++ src/main/java/com/aman/customthreadpool/Main.java
@@ -1,18 +1,19 @@
 package com.aman.customthreadpool;
 
-public class TestClass {
+public class Main {
     public static void main(String[] args) throws InterruptedException {
+        final int SIZE=3;
         SampleTask t1=new SampleTask(1);
         SampleTask t2=new SampleTask(2);
         SampleTask t3=new SampleTask(3);
-        MyThreadPool t=new MyThreadPool(2);
+        SampleTask t4=new SampleTask(4);
+        MyThreadPool t=new MyThreadPool(SIZE);
         t.execute(t1);
         t.execute(t2);
         t.execute(t3);
+        t.execute(t4);
         t.shutdown();
         while(!t.isTerminated()){
-
         }
-        System.out.println(t.isTerminated());
     }
 }
